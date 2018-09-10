26a64907a9af0e4fb2243eb788866c541cdbc48c
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-09-10 21:04:07 +0800

    给 this.submit 绑定一个 this

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,8 +3,8 @@ export default class TodoInput extends Component {
   render() {
     //input的值
     return <input type="text" defaultValue={this.props.content}
-    //回车事件
-      onKeyPress={this.submit} />
+      //回车事件  给 this.submit 绑定一个 this
+      onKeyPress={this.submit.bind(this)} />
   }
   submit(e) {
     if (e.key === 'Enter') {
