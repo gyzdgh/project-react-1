02882d3958208e3bfe79810b2fc44b76e4418095
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-09-10 20:52:44 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
- export default class TodoInput extends Component {
-  render(){
-    return <input type="text" defaultValue={this.props.content}/>
+export default class TodoInput extends Component {
+  render() {
+    //input的值
+    return <input type="text" defaultValue={this.props.content}
+    //回车事件
+      onKeyPress={this.submit} />
+  }
+  submit(e) {
+    if (e.key === 'Enter') {
+      console.log('用户按回车了');
+    }
   }
 }
