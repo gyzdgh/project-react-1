c5cd24a471c68b7a4a7ced843dc32d219bde7fc2
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-09-12 18:18:44 +0800

    拷贝 leancloud 初始化代码

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import * as localStore from './localStore'
 
+var APP_ID = 'ubdwH5WF0zfEIuyLXpUeScNn-gzGzoHsz';
+var APP_KEY = 'Kl1rB8pbFtT7Dc2G9GXUlyxk';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 
 class App extends Component {
   constructor(props) {
