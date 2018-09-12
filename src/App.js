e5452a6ce7ab6fb812e491aaa9b1931a9202c3fc
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-09-12 20:36:05 +0800

    添加登陆/注册对话框

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'normalize.css'
 import './reset.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
+import UserDialog from './UserDialog'
 
 class App extends Component {
   constructor(props) {
@@ -35,6 +36,7 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
+        <UserDialog />
       </div>
     );
   }
